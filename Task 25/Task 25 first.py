74d8764881aef9644d8a04afe864cc64d3afec43
nAMELESS <shushagei@gmail.com>
2024-10-10 22:10:13 +0300

    Add task 25

---
+++ Task 25/Task 25 first.py
@@ -0,0 +1,22 @@
+numbers=[]
+size=0
+for i in range(120115,120200+1):
+    numbers.append(i)
+    size+=1
+temp_start=0
+for i in range(len(numbers)):
+    if numbers[0]%numbers[i]==0:
+        temp_start+=1
+index=0
+max=numbers[0]
+for i in range(len(numbers)):
+    temp=0
+    for k in range(0, numbers[i]+1):
+        if numbers[i]%numbers[k]==0:
+            temp+=1
+    if temp>temp_start:
+        temp_start=temp
+        max=numbers[i]
+        index=i
+print('Index: ', index, 'Number: ', max)
+
