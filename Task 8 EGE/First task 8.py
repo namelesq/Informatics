825d417ba7afd71b98e1027ec7c26b828c19bb4c
nAMELESS <shushagei@gmail.com>
2024-10-10 22:04:00 +0300

    Add task

--- Task 8 EGE/First task 8.py
+++ Task 8 EGE/First task 8.py
@@ -1,1 +1,8 @@
-print("Hello world")
+from itertools import*
+index=0
+for i in product(sorted("УЦЮТПСОШ"), repeat=6):
+    string=''.join(i)
+    index+=1
+    if string=="ЮШОССО":
+        print(index)
+        break
